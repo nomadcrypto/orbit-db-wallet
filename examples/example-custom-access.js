14a334c1db5a0a0519219a6d8b018dcdebbcca60
= <nomadcrypto@gmail.com>
2019-03-16 13:38:23 -1000

    fixed support for new orbitdb identity provider api

--- examples/example-custom-access.js
+++ examples/example-custom-access.js
@@ -1,5 +1,23 @@
 const orbitWallet = require("../index");
 const Wallet = orbitWallet.Wallet;
+const IPFSAccessController = require("orbit-db-access-controllers/src/ipfs-access-controller")
+
+class OtherAccessController extends IPFSAccessController {
+
+  static get type () { return 'othertype' } // Return the type for this controller
+
+  async canAppend(entry, identityProvider) {
+    // Allow if access list contain the writer's publicKey or is '*'
+    const publicKey = entry.v === 0 ? entry.key : entry.identity.publicKey
+    console.log(publicKey)
+    if (this.write.includes(publicKey) ||
+      this.write.includes('*')) {
+      return true
+    }
+    return false
+  }
+}
+
 
 
 async function example(wallet) {
@@ -8,8 +26,21 @@ async function example(wallet) {
   const ipfs = await wallet.getIPFS()
   ipfs.on("error", (e) => console.error(e))
   ipfs.on('ready', async () => {
-    const orbitdb = await await wallet.getOrbitDB(ipfs);
-    const db = await orbitdb.log('hello')
+    let id = await ipfs.id();
+    console.log("ID", id)
+    let AccessControllers = require('orbit-db-access-controllers')
+    //let ac = new AccessControllers()
+    AccessControllers.addAccessController({ AccessController: OtherAccessController })
+    //console.log(ac)
+
+    const orbitdb = await wallet.getOrbitDB(ipfs, {AccessControllers: AccessControllers});
+    //console.log(orbitdb)
+    const db = await orbitdb.log('hello2', {
+      accessController : {
+        type: 'othertype',
+        write: [orbitdb.identity.publicKey]
+      }
+    })
 
     db.load()
     db.events.on('replicated', (address) => {
